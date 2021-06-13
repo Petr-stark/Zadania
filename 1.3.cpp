9ec9356732161c82b6f0ac30f7d2d95868a34012
Пётр <minysik76@gmail.com>
2021-06-13 20:36:21 +0300

    Задание 1.3

---
+++ 1.3.cpp
@@ -0,0 +1,36 @@
+﻿//1.3
+#include <iostream>
+//Подключение библиотеки ввода/вывода  
+#include <cmath>
+//Подключение математической библиотеки
+using namespace std;
+
+double energy (double m, const double c, double t, double t0);
+/**
+* \Payment-Расчет вес/сила тяжести.
+* \param  Q -энергия
+* \param  m- масса тела.
+* \param  c- удельная теплота плавления.
+* \param  t -начальная температура.
+* \param  t0 -конечная температура.
+* \return Возврат значения.
+*/
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double    m , t , t0,Q;
+	const double  c = 460.0;
+	cout << "Введите массу" << endl;
+	cin >> m;
+	cout << "Введите начальную температуру" << endl;
+	cin >> t;
+	cout << "Введите конечную температуру" << endl;
+	cin >> t0;
+	Q = energy (c,m,t,t0);
+	cout << " Энергия = " << Q << endl ;
+	return 0;
+}
+double energy (double m, const double c , double t , double t0)
+{
+	return c * m *( t0 - t );
+}
