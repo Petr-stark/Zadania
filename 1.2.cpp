57e589d9e090e96398c1ee27f4c53f311ffaae57
Пётр <minysik76@gmail.com>
2021-10-16 08:56:35 +0300

    1.Изменения

--- 1.2.cpp
+++ 1.2.cpp
@@ -2,44 +2,44 @@
 //1.2
 /**
 * \v скорость судна
-* \a скорость течения , если лодка против течени , тогда скорость течение вводим со знаком минус
+* \v2 скорость течения , если лодка против течени , тогда скорость течение вводим со знаком минус
 * \t время пути
-* \S путь лодки
+* \s путь лодки
 */
 #include <iostream>
 //подключение библиотеки ввода/вывода
 #include <cmath>
 //подключение математической библиотеки 
 using namespace std;
-double path(double a ,double t ,double b);
+double path(double a ,double t ,double v2);
 /**
 * \Путь- Функции пути.
 * \param v скорость судна(мы ее вводим).
 * \param t время(мы ее вводим).
-* \param a скорость реки(мы ее вводим).
-* \return Возвращает путь (S=(v + ( a )) * t ).
+* \param v2 скорость реки(мы ее вводим).
+* \return Возвращает путь (s=(v + ( v2 )) * t ).
 */
 int main()
 {
 	double v ; 
-	double a ;
+	double v2 ;
 	double t ;
-	double S ;
+	double s ;
 	setlocale(LC_ALL, "Russian");
 	cout << "Введите скорость судна\n";
 	cin >> v;
 	cout << "Введите время\n";
 	cin >> t;
 	cout << "Введите скорость реки\n";
-	cin >> a;
-	S = path(v , t , a );
-	cout << "Путь = " << S ;
+	cin >> v2;
+	s = path(v , t , v2 );
+	cout << "Путь = " << s ;
 
 	return 0;
 }
-double  path (const double v, const double t, const double a)
+double  path (const double v, const double t, const double v2)
 {
-	return (v +  a ) * t ;
+	return (v +  v2 ) * t ;
 }
 
 
