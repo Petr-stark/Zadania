f9ce62b7dd27b2dfa14e1e59887fd2273e87c779
Пётр <minysik76@gmail.com>
2021-11-23 20:00:17 +0300

    1.исправления

--- 2.3.cpp
+++ 2.3.cpp
@@ -14,7 +14,7 @@ using namespace std;
 * \param x Параметр r длина второго дома
 * \param a Параметр s ширина второго дома
 */
-bool CanBuid(const double a, const double b, const double p, const double q, const double r, const double s);
+bool areaLength(const double a, const double b, const double p, const double q, const double r, const double s);
 
 /**
 * \brief Точка входа в программу
@@ -24,45 +24,24 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	
 	double a, b, p, q, r, s;
 
-	cout << "Введите длину участка a = ";
-	cin >> a;
-
-	cout << "Введите ширину участка b = ";
-	cin >> b;
-
-	cout << "Введите размер длины первого дома p = ";
-	cin >> p;
-
-	cout << "Введите размера ширины первого дома q = ";
-	cin >> q;
-
-	cout << "Введите размера длины второго дома r = ";
-	cin >> r;
-
-	cout << "Введите размера ширины второго дома s = ";
-	cin >> s;
-
-	const auto y = CanBuid(a, b, p, q, r, s);
+	cout << "Введите значения : учаска , дома-1 , дома-2 a =   b =   p =   q =   r =   s =  " << "\n";
+	cin >> a >> b >> p >> q >> r >> s;
+	const auto y = areaLength(a, b, p, q, r, s);
 
 	if (y)
-
 	{
 		cout << "Могут быть установленны 2 дома" << endl;
 	}
-
 	else
-
 	{
 		cout << "Не могут быть установлены 2 дома" << endl;
 	}
-
 	return 0;
 }
 
-bool CanBuid(const double a, const double b, const double p, const double q, const double r, const double s)
+bool areaLength(const double a, const double b, const double p, const double q, const double r, const double s)
 {
-	return	((a > p + r) || (a > p + s) || (a > q + r) || (a > q + s)) && ((b > p + r) || (b > p + s) || (b > q + r) || (b > q + s));
+	return	((a >= p + r) || (a >= p + s) || (a >= q + r) || (a >= q + s)) && ((b >= p + r) || (b >= p + s) || (b >= q + r) || (b >= q + s));
 }
