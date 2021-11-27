474479419f9b8c4e52394bad0129deaa629bdf6e
Пётр <minysik76@gmail.com>
2021-11-27 13:07:38 +0300

    1.изменения

--- программа.cpp
+++ программа.cpp
@@ -1,6 +1,4 @@
-﻿// 1.1
-// библиотеки нужные для мат. вычислений
-#define _USE_MATH_DEFINES 
+﻿#define _USE_MATH_DEFINES 
 #include <iostream>
 #include <cmath> 
 // нужно чтобы не прописывать std 
@@ -12,11 +10,8 @@ using namespace std;
 * \param z параметр z ,задан в задании
 * \return возвращаем значение равное : a
 */
-double GetA(const double x, const double y, const double z)
-{
-	// функция для вычисления переменной а
-	return (pow(z, 2) * x + pow(M_E, -x) * cos(y * x)) / (y * x - pow(M_E, -x) * sin(y * x) + 1);
-}
+double GetA(const double x, const double y, const double z);
+
 /**
 * \GetB Функция для расчета b
 * \param x параметр x ,задан в задании
@@ -24,11 +19,8 @@ double GetA(const double x, const double y, const double z)
 * \param z параметр z ,задан в задании
 * \return возвращаем значение равное : b
 */
-double GetB(const double x, const double y, const double z)
-{
-	return pow(M_E, 2 * x) * log(z + x) - pow(y, 3 * x) * log(y - x);
-	// функция для вычисления переменной b
-}
+double GetB(const double x, const double y, const double z);
+
 // Объявление функции main()
 int main()
 {
@@ -39,4 +31,16 @@ int main()
 	const auto b = GetB(x, y, z);
 	cout << "x = " << x << "\ny = " << y << "\nz = " << z << "\na = " << a << "\nb = " << b;  // вывод данных ,вывод на новою строчку
 	return 0;
+}
+
+double GetA(const double x, const double y, const double z)
+{
+	// функция для вычисления переменной а
+	return (pow(z, 2) * x + pow(M_E, -x) * cos(y * x)) / (y * x - pow(M_E, -x) * sin(y * x) + 1); // так же можно было использовать exp  exp(x) вместо pow(a,b)
+}
+
+double GetB(const double x, const double y, const double z)
+{
+	return  exp(2*x)* log(z + x) - pow(y, 3 * x) * log(y - x);//  можно было использовть вместо exp(2*x) = pow(M_E, 2 * x) 
+	// функция для вычисления переменной b
 }
